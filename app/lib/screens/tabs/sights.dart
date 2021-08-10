a89fec62dbea7207005bf24853df49cbb25bfb8f
Arnav <arnavbansal101@gmail.com>
2021-08-10 10:21:07 +0530

    add tabs

---
+++ app/lib/screens/tabs/sights.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class Sights extends StatefulWidget {
+  const Sights({Key? key}) : super(key: key);
+
+  @override
+  _SightsState createState() => _SightsState();
+}
+
+class _SightsState extends State<Sights> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ app/lib/screens/tabs/utilities.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+class Utilities extends StatefulWidget {
+  const Utilities({ Key? key }) : super(key: key);
+
+  @override
+  _UtilitiesState createState() => _UtilitiesState();
+}
+
+class _UtilitiesState extends State<Utilities> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
