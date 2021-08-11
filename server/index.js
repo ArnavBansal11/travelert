d5c9df3b672f802280b7a7c0412a9de7ee1d7603
Arnav <arnavbansal101@gmail.com>
2021-08-11 17:11:48 +0530

    add route to get district id

--- server/index.js
+++ server/index.js
@@ -1,39 +1,64 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const places = require('./agra-delhi.json');
+const places = require("./data/agra-delhi.json");
+const pincodes = require("./data/pincodes.json");
+const districts = require("./data/data.json");
 
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, x-auth-token'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-auth-token"
   );
   next();
 });
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/api/touristSites/:districtID', (req, res) => {
+app.get("/api/touristSites/:districtID", (req, res) => {
   const districtID = req.params.districtID;
   const touristSites = places[districtID].touristSights;
   res.json(touristSites);
 });
 
-app.get('/api/utilities/:districtID', (req, res) => {
+app.get("/api/utilities/:districtID", (req, res) => {
   const districtID = req.params.districtID;
   const utilities = places[districtID].utils;
   res.json(utilities);
+});
+
+app.get("/api/toDistrict/:pincode", (req, res) => {
+  const p = req.params.pincode;
+
+  let pincode;
+  pincodes.forEach((pin) => {
+    if (pin.pincode == p) {
+      pincode = pin;
+    }
+  });
+
+  let district;
+  Object.values(districts).forEach((s) => {
+    s.districts.forEach((d) => {
+      if (d.district_name == pincode.districtName) {
+        console.log(d);
+        district = d;
+      }
+    });
+  });
+
+  res.send(district);
 });
 
 const port = process.env.PORT || 3000;
