0ab94d74aadffe982fda2daf9a3a7b99849f0bbd
Arnav <arnavbansal101@gmail.com>
2021-08-14 17:25:23 +0530

    nearby coords alg done :D

--- server/index.js
+++ server/index.js
@@ -107,11 +107,43 @@ app.get("/api/getNearby/:lat/:long/:pin", (req, res) => {
 
     const dId = district.district_id;
 
-    res.send(places[dId].touristSights ?? []);
+    const cands = places[dId].touristSights ?? [];
+
+    const nearby = [];
+    const lat = req.params.lat;
+    const long = req.params.long;
+
+    cands.forEach((c) => {
+      const dist = getDistanceFromLatLonInKm(lat, long, c.lat, c.long);
+      if (dist <= 1) {
+        nearby.push(c);
+      }
+    });
+
+    res.send(nearby);
   } catch (e) {
     res.send([]);
   }
 });
+
+function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) *
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
